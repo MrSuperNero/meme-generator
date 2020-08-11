cbecc50ef7c1796f4f12508f7f889447eb0e2d49
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-08-11 00:53:23 -0400

    added build folder

---
+++ build/precache-manifest.d6b24f7c797110d9f552acc0a3121fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc7f86d26c33b1f2270073f09372748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dbe47ca5fa357c09a65",
+    "url": "/static/css/main.23e75a61.chunk.css"
+  },
+  {
+    "revision": "0a2e1598a5f2998ac8f9",
+    "url": "/static/js/2.8f045b6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f045b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dbe47ca5fa357c09a65",
+    "url": "/static/js/main.66b12daf.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  }
+]);
